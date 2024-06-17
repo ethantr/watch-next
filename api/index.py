4c179e0713f4736118526ab83dc561dcde2e68e9
Ethan Teber-Rossi <ethantrossi@gmail.com>
2024-06-17 20:08:15 +1000

    Playground with tmdb

--- api/index.py
+++ api/index.py
@@ -1,6 +1,18 @@
-from flask import Flask,jsonify
+import os
+from flask import Flask, jsonify
+import requests
+from dotenv import load_dotenv
 app = Flask(__name__)
 
+tmdb_api_key = os.getenv('TMDB_API_KEY')
+
+@app.route('/api/movies', methods=['GET'])
+def get_movies():
+    print(tmdb_api_key)
+    url = 'https://api.themoviedb.org/3/movie/popular?api_key='+tmdb_api_key
+    response = requests.get(url)
+    data = response.json()
+    return jsonify(data)
 
 # /api/home
 @app.route("/api/home",methods=["GET"])
@@ -13,7 +25,9 @@ def return_home():
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+    return jsonify({"message": tmdb_api_key, 
+                    "people":["greg 1","greg2"]
+                    })
 
 if __name__ == "__main__":
     app.run(debug = True,port = 8080)
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,43 @@
-"use client"
+"use client"; // This directive enables client-side rendering for this component
+
 import { useEffect, useState } from 'react';
-interface Data {
-  message: string;
+
+interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  vote_average: number;
 }
 
 const Home: React.FC = () => {
-  const [data, setData] = useState<Data | null>(null);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-    fetch('/api/data')
-      .then((response) => response.json())
-      .then((data: Data) => setData(data));
+    fetch('/api/movies')
+      .then(response => response.json())
+      .then(data => setMovies(data.results));
   }, []);
 
   return (
     <div>
-      <h1>Next.js with Flask</h1>
-      {data ? <p>{data.message}</p> : <p>Loading...</p>}
+      <h1>Popular Movies</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+        {movies.map(movie => (
+          <div key={movie.id} style={{ width: '200px' }}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              style={{ width: '100%' }}
+            />
+            <h3>{movie.title}</h3>
+            <p>Release Date: {new Date(movie.release_date).toDateString()}</p>
+            <p>Rating: {movie.vote_average}</p>
+            <p>{movie.overview}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
